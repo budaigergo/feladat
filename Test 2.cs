7d74f205d260d1350a0b19ba593fe84d9c748217
budaigergo <112852798+budaigergo@users.noreply.github.com>
2022-09-21 13:17:46 +0200

    2.0

---
+++ Test 2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Adj meg kettő egész számot!:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.ReadLine();
+        }
+    }
+}
