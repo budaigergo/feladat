1e46510898981fbd1278b4ed92d0ab5ea1082e7e
budaigergo <112852798+budaigergo@users.noreply.github.com>
2022-09-21 13:18:02 +0200

    3.0

---
+++ Test 3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Adj meg kettő egész számot!:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());eadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine("A nagyobb szám:{0}", a);
+            }
+            else { Console.WriteLine("A nagyobb szám:{0}", b); }
+
+            Console.ReadLine();
+        }
+    }
+}
